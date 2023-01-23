2cdb5ff63d024a2121ca98944f1cf3164a814ae2
Jip Irfandy <irfandyjip98@gmail.com>
2023-01-23 13:37:52 +0700

    feat(pages): integrate `HomePage` with `ArticlesCell`

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -1,7 +1,10 @@
 export const QUERY = gql`
   query ArticlesQuery {
-    articles {
+    articles: posts {
       id
+      title
+      body
+      createdAt
     }
   }
 `
@@ -16,10 +19,16 @@ export const Failure = ({ error }) => (
 
 export const Success = ({ articles }) => {
   return (
-    <ul>
-      {articles.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
+    <>
+      {articles.map((article) => (
+        <article key={article.id}>
+          <header>
+            <h2>{article.title}</h2>
+          </header>
+          <p>{article.body}</p>
+          <div>Posted at: {article.createdAt}</div>
+        </article>
+      ))}
+    </>
   )
 }
